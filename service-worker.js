b496853974000c5332b86568f9d3f13c8a3b29de
Ionut Birlad <birladionut@yahoo.it>
2020-07-31 15:10:11 +0200

    modifica metatags

--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.a2bb69b9d86846ff36410c3a72b135a8.js"
+  "/precache-manifest.ef131d0a26e3ef49c337f0f2a063c17e.js"
 );
 
 workbox.core.setCacheNameDetails({prefix: "vue-argon-design-system"});
